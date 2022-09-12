935c37fb2a2969e19ac05822dfa34f7d923c146d
Shirendev08 <shirendev.07@gmail.com>
2022-09-12 15:49:15 +0800

    a

--- my-ap1/src/App.js
+++ my-ap1/src/App.js
@@ -1,17 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const Person = (props) => {
+  return (
+    <>
+    <h1>Name :{props.name} </h1>
+    <h2> Last Name :{props.lastname}  </h2>
+    <h2>Age : {props.age} </h2>
+    </>
+  )
+}
+
+
 
 
 const App = () => {
   const [counter, setCounter] = useState(0);
   
+  useEffect(()=>{
+    alert("You have changed counter to " + counter)
+  }, [counter])
   return (
     <div className="App">
-      <button onClick={()=> setCounter(-5) }>-</button>
+      <button onClick={ ()=> setCounter(prevCount => prevCount-1) }>-</button>
       <h1>{counter}</h1>
-      <button>+</button>
+      <button onClick={ ()=> setCounter(prevCount => prevCount+1) }>+</button>
+      <Person name="John" lastname="Doe" age={2+2}/>
     </div>
+    
   );
 }
 

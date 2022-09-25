00d961a555faa0a305d7f5d6741d87d1e74fe23f
Shirendev <shirendev.07@gmail.com>
2022-09-25 12:27:35 +0800

    finish

--- my-ap2/src/App.js
+++ my-ap2/src/App.js
@@ -1,5 +1,7 @@
 import Home from "./pages/Home/Home"
-
+// import Profile from "./pages/Home/profile/Profile"
+// import Login from "./pages/Home/login/Login"
+// import Register from "./pages/Home/register/Register"
 function App() {
   return <Home/>
 }

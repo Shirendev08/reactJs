2d06a5259be41cc5c5021d88993d58b8b273e04c
Shirendev08 <shirendev.07@gmail.com>
2022-09-26 15:25:42 +0800

    A

--- my-ap2/src/App.js
+++ my-ap2/src/App.js
@@ -1,9 +1,9 @@
 import Home from "./pages/Home/Home"
-// import Profile from "./pages/Home/profile/Profile"
+import Profile from "./pages/Home/profile/Profile"
 // import Login from "./pages/Home/login/Login"
 // import Register from "./pages/Home/register/Register"
 function App() {
-  return <Home/>
+  return <Profile/>
 }
 
 export default App;

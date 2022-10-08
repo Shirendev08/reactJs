12d9e9d0c768ca61bd202797814216224cc7f2e7
Shirendev <shirendev.07@gmail.com>
2022-10-08 10:16:32 +0800

    first

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,25 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c0a2d3b7a8mshe348abd873a4e4ep17385djsn44c42053c9a5',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+
+
+// export const geoApiOptions = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'c0a2d3b7a8mshe348abd873a4e4ep17385djsn44c42053c9a5',
+// 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));

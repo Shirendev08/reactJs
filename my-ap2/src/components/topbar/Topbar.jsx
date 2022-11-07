f16481e6400137b5876bba8d1c46e3167325105b
Shirendev08 <tsendbaterdene7@gmail.com>
2022-11-07 17:33:51 +0800

    a

--- my-ap2/src/components/topbar/Topbar.jsx
+++ my-ap2/src/components/topbar/Topbar.jsx
@@ -1,11 +1,38 @@
 import "./topbar.css"
 import { Search, Person, Chat, Notifications } from "@material-ui/icons"
+import { useState, useEffect } from "react";
 export default function Topbar() {
+  const [datas, setDatas] = useState();
+  const urlLookup = "http://btax.mandakh.org:8000/lookup/";
+  useEffect(() => {
+    const bodyChiglel = {
+      action: "chiglel",
+    };
+    sendRequest(urlLookup, bodyChiglel);
+  }, []);
+
+  const sendRequest = async (url, body) => {
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then(async (response) => {
+        setDatas(response);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+   console.log(datas)
   return (
     <div className="topbarContainer">
 
       <div className="topbarLeft">
-        <span className="logo">Lamasocial</span>
+        <span className="logo">{datas && datas.data[0].chiglelname}</span>
       </div>
       <div className="topbarCenter">
         <div className="searchBar">
--- my-ap2/src/pages/Home/Home.jsx
+++ my-ap2/src/pages/Home/Home.jsx
@@ -2,10 +2,15 @@ import Rightbar from "../../components/rightbar/Rightbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
 import Feed from "../../components/feed/Feed";
+import { useState, useEffect } from "react";
 import "./Home.css"
 export default function Home() {
+
+  
   return (
     <>
+   
+
     <Topbar/>
     <div className="homeContainer">
     <Sidebar/>
